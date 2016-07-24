582e631a065d0a2fe20f99d08ad6071e1f6c4ea2
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-07-24 23:20:55 +0200

    removed useless tests from DictionaryExtensionMethods

--- src/Compiler/Tools/DictionaryExtensionMethods.cs
+++ src/Compiler/Tools/DictionaryExtensionMethods.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace REC.Tools
 {
     public static class DictionaryExtensionMethods
     {
         public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<TValue> valueFactory) {
-            if (dictionary == null) throw new ArgumentNullException("dictionary");
-            if (key == null) throw new ArgumentNullException("key");
-            if (valueFactory == null) throw new ArgumentNullException("valueFactory");
+            //if (dictionary == null) throw new ArgumentNullException("dictionary");
+            //if (key == null) throw new ArgumentNullException("key");
+            //if (valueFactory == null) throw new ArgumentNullException("valueFactory");
 
             if (dictionary.ContainsKey(key)) return dictionary[key];
             var value = valueFactory();
             dictionary[key] = value;
             return value;
+        }
+
+        public static TValue GetOr<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<TValue> notFound)
+        {
+            //if (dictionary == null) throw new ArgumentNullException("dictionary");
+            //if (key == null) throw new ArgumentNullException("key");
+            //if (notFound == null) throw new ArgumentNullException("valueFactory");
+
+            return dictionary.ContainsKey(key) ? dictionary[key] : notFound();
         }
     }
 }
