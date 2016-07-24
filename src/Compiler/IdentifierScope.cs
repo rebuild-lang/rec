d8027d5dc531fa892543c40986612a84d9115988
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-07-24 23:22:00 +0200

    Extended IdentifierScope and added tests

--- src/Compiler/IdentifierScope.cs
+++ src/Compiler/IdentifierScope.cs
@@ -1,19 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using REC.Scanner;
+using REC.Tools;
 
 namespace REC
 {
     using Dict = Dictionary<string, IIdentifier>;
+    using DictKeyValue = KeyValuePair<string, IIdentifier>;
 
-    public class IdentifierScope
+    public delegate void ScopedIdentifierAdded(IIdentifierScope scope, IIdentifier identifier);
+
+    public delegate void ScopeDisposed(IIdentifierScope scope);
+
+    public interface IIdentifierScope : IDisposable
+    {
+        IIdentifierScope Parent { get; }
+        IIdentifier this[string key] { get; }
+
+        event ScopedIdentifierAdded IdentifierAdded;
+        event ScopeDisposed Disposed;
+
+        bool Add(IIdentifier identifier);
+    }
+
+    internal class IdentifierScope : IIdentifierScope, IEnumerable<IIdentifier>
     {
         private readonly Dict _identifiers = new Dict();
-        private readonly IdentifierScope _parent;
+        private readonly IIdentifierScope _parent;
 
-        public IIdentifier this[string key] => _identifiers[key] ?? _parent?[key];
+        public IIdentifierScope Parent => _parent;
 
-        public IdentifierScope(IdentifierScope parent = null) {
+        public event ScopedIdentifierAdded IdentifierAdded;
+        public event ScopeDisposed Disposed;
+
+        public IIdentifier this[string key] => _identifiers.GetOr(key, () => _parent?[key]);
+
+        public IdentifierScope(IIdentifierScope parent = null) {
             _parent = parent;
+            if (_parent != null)
+            {
+                _parent.Disposed += OnParentDisposed;
+                _parent.IdentifierAdded += OnParentIdentifierAdded;
+            }
+        }
+
+        private void OnParentDisposed(IIdentifierScope _) {
+            Dispose();
+        }
+
+        private void OnParentIdentifierAdded(IIdentifierScope scope, IIdentifier identifier)
+        {
+            if (_identifiers.ContainsKey(identifier.Label)) return;
+            IdentifierAdded?.Invoke(scope, identifier);
+        }
+
+        public void Dispose() {
+            if (_parent != null) _parent.Disposed -= OnParentDisposed;
+            Disposed?.Invoke(this);
         }
 
         public bool Add(IIdentifier identifier) {
@@ -21,7 +65,49 @@ namespace REC
             if (_identifiers.ContainsKey(label))
                 return false;
             _identifiers.Add(label, identifier);
+            IdentifierAdded?.Invoke(this, identifier);
             return true;
+        }
+
+        public IEnumerator<IIdentifier> GetEnumerator() {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        [Serializable]
+        public struct Enumerator : IEnumerator<IIdentifier>
+        {
+            readonly IdentifierScope _startScope;
+            IdentifierScope _scope;
+            Dict.ValueCollection.Enumerator _enumerator;
+
+            internal Enumerator(IdentifierScope scope) : this() {
+                _startScope = scope;
+                Reset();
+            }
+
+            public bool MoveNext() {
+                while (!_enumerator.MoveNext()) {
+                    _scope = (IdentifierScope)_scope?._parent;
+                    if (null == _scope) return false;
+                    _enumerator = _scope._identifiers.Values.GetEnumerator();
+                }
+                return true;
+            }
+
+            public void Reset() {
+                _scope = _startScope;
+                _enumerator = _scope._identifiers.Values.GetEnumerator();
+            }
+
+            public IIdentifier Current => _enumerator.Current;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() {}
         }
     }
 }
---
+++ test/Compiler.Tests/IdentifierScopeTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using REC.Scanner;
+
+namespace REC.Tests
+{
+    [TestFixture()]
+    public class IdentifierScopeTests
+    {
+        internal class Identifier : IIdentifier
+        {
+            public string Label { get; set; }
+        }
+
+        [Test()]
+        public void IdentifierScope() {
+            var parentScope = new IdentifierScope();
+            var childScope = new IdentifierScope(parentScope);
+
+            Assert.AreEqual(childScope.Parent, parentScope);
+            Assert.IsNull(parentScope.Parent);
+
+            var childScopeDisposed = false;
+            childScope.Disposed += scope => {
+                Assert.AreEqual(childScope, scope);
+                childScopeDisposed = true;
+            };
+            var parentScopeDisposed = false;
+            parentScope.Disposed += scope =>
+            {
+                // ReSharper disable once AccessToDisposedClosure
+                Assert.AreEqual(parentScope, scope);
+                parentScopeDisposed = true;
+            };
+
+            parentScope.Dispose();
+
+            Assert.IsTrue(parentScopeDisposed);
+            Assert.IsTrue(childScopeDisposed);
+        }
+
+        [Test()]
+        public void Add() {
+            var parentScope = new IdentifierScope();
+            var childScope = new IdentifierScope(parentScope);
+            var myScope = new IdentifierScope(childScope) {new Identifier {Label = "label"}};
+
+            var testIdentifier = new Identifier { Label = "label" };
+            var parentIdentifierAdded = false;
+            parentScope.IdentifierAdded += (scope, identifier) =>
+            {
+                Assert.AreEqual(parentScope, scope);
+                Assert.AreEqual(testIdentifier, identifier);
+                parentIdentifierAdded = true;
+            };
+
+            var childIdentifierAdded = false;
+            childScope.IdentifierAdded += (scope, identifier) => {
+                Assert.AreEqual(parentScope, scope);
+                Assert.AreEqual(testIdentifier, identifier);
+                childIdentifierAdded = true;
+            };
+
+            var myIdentifierAdded = false;
+            childScope.IdentifierAdded += (scope, identifier) => childIdentifierAdded = true;
+
+            var result = parentScope.Add(testIdentifier);
+            Assert.IsTrue(result);
+            Assert.IsFalse(parentScope.Add(testIdentifier));
+
+            Assert.IsTrue(parentIdentifierAdded);
+            Assert.IsTrue(childIdentifierAdded);
+            Assert.IsFalse(myIdentifierAdded);
+            Assert.AreEqual(testIdentifier, childScope[testIdentifier.Label]);
+            Assert.AreNotEqual(testIdentifier, myScope[testIdentifier.Label]);
+        }
+
+        [Test()]
+        public void GetEnumerator() {
+            var parentScope = new IdentifierScope() { new Identifier { Label = "label" } };
+            var childScope = new IdentifierScope(parentScope);
+            var myScope = new IdentifierScope(childScope) { new Identifier { Label = "fun" } };
+
+            var result = myScope.Select(i => i.Label).ToList();
+
+            Assert.AreEqual(new List<string>() { "fun", "label" }, result);
+        }
+    }
+}
