e247e57b389b096772c6ab9d1ecc0a73f94262c8
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-08-18 21:54:31 +0200

    extended comment filter to filter adjacent newlines with optional indentations as well

--- src/Compiler/Parser/CommentIndentationFilter.cs
+++ src/Compiler/Parser/CommentIndentationFilter.cs
@@ -5,8 +5,11 @@ namespace REC.Parser
 {
     public static class CommentIndentationFilter
     {
-        // filters out all comments and also indentations preceding comments
-        // this is useful to prevent the indentation checker from complaining about comments
+        // filters out 
+        // * all comments 
+        // * newline + indentations preceding comments
+        // * two adjacent newline + indentations (basically multiple newlines)
+        // this simplifies the evaluation of the token stream
         public static IEnumerable<TokenData> Filter(IEnumerable<TokenData> input) {
             using (var it = input.GetEnumerator()) {
                 do {
@@ -14,14 +17,21 @@ namespace REC.Parser
                 } while (it.Current.Type == Token.Comment); // skip initial comments
                 var previous = it.Current;
                 while (it.MoveNext()) {
-                    if (it.Current.Type != Token.Comment) yield return previous;
-                    else {
-                        if (previous.Type != Token.NewLineIndentation) yield return previous; // skip indentation before comment
-                        if (!it.MoveNext()) yield break; // skip final comment
+                    switch (it.Current.Type) {
+                        case Token.Comment: // skip any comment
+                            if (previous.Type != Token.NewLineIndentation) yield return previous; // skip indentation before comment
+                            if (!it.MoveNext()) yield break; // skip final comment / indentation
+                            break;
+                        case Token.NewLineIndentation:
+                            if (previous.Type != Token.NewLineIndentation) yield return previous; // skip double newlines
+                            break;
+                        default:
+                            yield return previous; // keep everything else
+                            break;
                     }
                     previous = it.Current;
                 }
-                yield return previous;
+                if (previous.Type != Token.NewLineIndentation) yield return previous;
             }
         }
     }
--- test/Compiler.Tests/Parser/CommentIndentationFilterTests.cs
+++ test/Compiler.Tests/Parser/CommentIndentationFilterTests.cs
@@ -11,16 +11,39 @@ namespace REC.Tests.Parser
     {
         [TestCase(arg1: new[] {
             Token.Comment,
-            Token.NewLineIndentation,
+            Token.NewLineIndentation, Token.IdentifierLiteral
         }, arg2: new[] {
-            Token.NewLineIndentation
-        })]
+            Token.NewLineIndentation, Token.IdentifierLiteral
+        }, TestName = "Filter starting comment")]
         [TestCase(arg1: new[] {
             Token.NewLineIndentation, Token.Comment,
+            Token.NewLineIndentation, Token.IdentifierLiteral
+        }, arg2: new[] {
+            Token.NewLineIndentation, Token.IdentifierLiteral,
+        }, TestName = "Filter indented comment")]
+        [TestCase(arg1: new[] {
             Token.NewLineIndentation, Token.IdentifierLiteral, Token.Comment,
         }, arg2: new[] {
             Token.NewLineIndentation, Token.IdentifierLiteral,
-        })]
+        }, TestName = "Filter final comment")]
+        [TestCase(arg1: new[] {
+            Token.NewLineIndentation,
+            Token.NewLineIndentation, Token.IdentifierLiteral,
+        }, arg2: new[] {
+            Token.NewLineIndentation, Token.IdentifierLiteral,
+        }, TestName = "Filter newline sequence")]
+        [TestCase(arg1: new[] {
+            Token.NewLineIndentation,
+            Token.NewLineIndentation,
+            Token.NewLineIndentation, Token.IdentifierLiteral,
+        }, arg2: new[] {
+            Token.NewLineIndentation, Token.IdentifierLiteral,
+        }, TestName = "Filter long newline sequence")]
+        [TestCase(arg1: new[] {
+            Token.NewLineIndentation, Token.IdentifierLiteral, Token.NewLineIndentation,
+        }, arg2: new[] {
+            Token.NewLineIndentation, Token.IdentifierLiteral,
+        }, TestName = "Filter final newline")]
         public void Filter(IEnumerable<Token> inputTokens, IEnumerable<Token> expectedTokens) {
             var input = inputTokens.Select(t => new TokenData {Type = t});
             var expected = expectedTokens.Select(t => new TokenData {Type = t});
