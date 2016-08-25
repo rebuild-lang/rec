5298b066788422e5527b8434a89507f80c63f610
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-08-25 14:25:03 +0200

    added BlockLineGrouping

---
+++ src/Compiler/Parser/BlockLineGrouping.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using REC.Scanner;
+using REC.Tools;
+
+namespace REC.Parser
+{
+    public interface ITokenLine
+    {
+        IList<TokenData> Tokens { get; }
+    }
+
+    public interface ITokenBlock
+    {
+        IList<ITokenLine> Lines { get; }
+    }
+
+    /**
+     * Preparation step that allows to defer block processing
+     * 
+     * * Lines are joined with all continuations
+     * * BlockStart gets ITokenBlock as Data containing all lines
+     * * Semicolons are splitten into separate lines
+     * 
+     * example:S
+     *   'if'[WS]'a'[NewLine
+     *      ]'&&'[WS]'b'[BlockStart
+     *     ]'print'[WS]"a"[NewLine
+     *   ]'else'[BlockStart
+     *     ]'print'[WS]"b"[BlockEnd
+     *   ]
+     * 
+     *  is turned into:
+     *   Lines:
+     *   - - 'if'
+     *     - [WS]
+     *     - 'a'
+     *     - [WS]
+     *     - '&&'
+     *     - [WS]
+     *     - 'b'
+     *     - BlockStart:
+     *         Lines:
+     *         - - 'print'
+     *           - [WS]
+     *           - "a"
+     *     - 'else'
+     *     - BlockStart:
+     *         Lines:
+     *         - - 'print'
+     *           - [WS]
+     *           - "b"
+     */
+    public class BlockLineGrouping
+    {
+        class Line : ITokenLine
+        {
+            public IList<TokenData> Tokens { get; } = new List<TokenData>();
+        }
+
+        class Block : ITokenBlock
+        {
+            public IList<ITokenLine> Lines { get; } = new List<ITokenLine>();
+        }
+
+        char _indentChar = '\0';
+        bool _done; // marks that we hit the end of input
+
+        public ITokenBlock Group(IEnumerable<TokenData> input) {
+            _indentChar = '\0';
+            _done = false;
+            using (var it = input.GetEnumerator()) {
+                if (!it.MoveNext()) return new Block();
+                var blockColumn = 1;
+                if (it.Current.Type == Token.NewLineIndentation
+                    || it.Current.Type == Token.WhiteSpaceSeperator) { // first whitespace is an indentation
+                    blockColumn = GetIndent(it);
+                    if (!it.MoveNext()) return new Block();
+                }
+                var block = ParseBlock(it, blockColumn);
+                if (!_done) {
+                    // TODO: report extra lines
+                }
+                return block;
+            }
+        }
+
+        ITokenBlock ParseBlock(IEnumerator<TokenData> it, int blockColumn) {
+            var block = new Block();
+            while (true) {
+                while (true) {
+                    int indent;
+                    switch (it.Current.Type) {
+                        case Token.BlockEndIndentation:
+                            indent = GetIndent(it);
+                            if (indent < blockColumn) return block; // do not consume parent end block
+                            // TODO report misplaced end
+                            // handling: ignore it
+                            if (!it.MoveNext()) {
+                                _done = true;
+                                return block;
+                            }
+                            continue;
+                        case Token.BlockStartIndentation: // this occurs if block starts with the wrong indentation
+                        case Token.NewLineIndentation:
+                            indent = GetIndent(it);
+                            if (indent < blockColumn) return block; // line is not part of this block
+                            if (indent > blockColumn) {
+                                // TODO: report indentation error
+                                // handling: take the line into this block
+                            }
+                            if (!it.MoveNext()) {
+                                _done = true;
+                                return block;
+                            }
+                            continue;
+                    }
+                    break;
+                }
+                var line = ParseLine(it, blockColumn);
+                if (line != null) block.Lines.Add(line);
+                if (_done) break;
+            }
+            return block;
+        }
+
+        ITokenLine ParseLine(IEnumerator<TokenData> it, int parentBlockColumn) {
+            var line = new Line();
+            var expectEnd = false;
+            while (true) {
+                ExtractLineTokens(it, line);
+                if (_done) return line;
+
+                while (true) {
+                    var nextColumn = GetIndent(it);
+                    TokenData current;
+                    switch (it.Current.Type) {
+                        case Token.NewLineIndentation:
+                            if (nextColumn < parentBlockColumn) return line; // end of the parent block
+                            // line continuation
+                            while (true) {
+                                if (!line.Tokens.IsEmpty() && line.Tokens.Last().Type != Token.BlockStartIndentation) {
+                                    current = it.Current;
+                                    current.Type = Token.WhiteSpaceSeperator;
+                                    line.Tokens.Add(current);
+                                }
+                                if (!it.MoveNext()) _done = true;
+                                if (_done) return line; // should not happen (final NewLineIndentation was filtered)
+                                ExtractLineTokens(it, line);
+                                if (_done) return line;
+
+                                if (it.Current.Type != Token.NewLineIndentation) break;
+                                //var continueColumn = GetIndent(it);
+                                //if (continueColumn >= nextColumn) continue; // next line is part of the continuation
+                                // TODO report continuation error
+                                // TODO handling: add lines to a block as well
+                            }
+                            continue;
+                        case Token.BlockEndIndentation:
+                            if (nextColumn < parentBlockColumn) {
+                                if (expectEnd) {
+                                    // TODO report missing end
+                                    // handling terminate line anyways
+                                }
+                                return line; // end of the parent block
+                            }
+                            if (nextColumn == parentBlockColumn) {
+                                if (!expectEnd) {
+                                    // TODO report unexpected end
+                                    // handling terminate line anyways
+                                }
+                                if (!it.MoveNext()) _done = true; // consume the end
+                                return line;
+                            }
+                            // TODO report nested end
+                            // handling: ignored
+                            if (!it.MoveNext()) {
+                                _done = true;
+                                return line;
+                            }
+                            continue;
+                        case Token.BlockStartIndentation:
+                            expectEnd = true;
+                            current = it.Current;
+                            if (nextColumn < parentBlockColumn) {
+                                // TODO report missing end
+                                // handling: add empty block and finish line
+                                current.Data = new Block();
+                                line.Tokens.Add(current);
+                                return line;
+                            }
+                            if (nextColumn == parentBlockColumn) { // empty block
+                                current.Data = new Block();
+                                line.Tokens.Add(current);
+                                if (!it.MoveNext()) _done = true;
+                                continue;
+                            }
+                            current.Data = ParseBlock(it, nextColumn);
+                            line.Tokens.Add(current);
+                            continue;
+                    }
+                    break;
+                }
+            }
+        }
+
+        void ExtractLineTokens(IEnumerator<TokenData> it, ITokenLine line) {
+            var current = it.Current;
+            while (current.Type != Token.NewLineIndentation
+                   && current.Type != Token.BlockStartIndentation
+                   && current.Type != Token.BlockEndIndentation) {
+                line.Tokens.Add(current);
+                if (!it.MoveNext()) {
+                    _done = true;
+                    return;
+                }
+                current = it.Current;
+            }
+        }
+
+        int GetIndent(IEnumerator<TokenData> it) {
+            var range = it.Current.Range;
+            if (range == null) return 1;
+            if (range.Length != 0) {
+                var text = range.Text;
+                if (_indentChar == '\0') {
+                    _indentChar = text[index: 0];
+                }
+                if (text.Any(x => x != _indentChar)) { } // TODO warning
+            }
+            return range.End.Column;
+        }
+    }
+}
---
+++ test/Compiler.Tests/Parser/BlockLineGroupingTests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using REC.Scanner;
+
+namespace REC.Parser.Tests
+{
+    [TestFixture()]
+    public class BlockLineGroupingTests
+    {
+        public struct TestData
+        {
+            public IEnumerable<TokenData> Input;
+            public ITokenBlock Output;
+        }
+
+        static TokenData Id(string text) {
+            return new TokenData {
+                Type = Token.IdentifierLiteral,
+                Range = new TextFileRange {
+                    File = new TextFile { Content = text },
+                    End = new TextPosition { Column = text.Length, Index = text.Length }
+                }
+            };
+        }
+
+        static TokenData StringLiteral(string text) {
+            return new TokenData {
+                Type = Token.StringLiteral,
+                Range = new TextFileRange {
+                    File = new TextFile { Content = text },
+                    End = new TextPosition { Column = text.Length, Index = text.Length }
+                }
+            };
+        }
+
+        static TokenData WS() {
+            return new TokenData { Type = Token.WhiteSpaceSeperator };
+        }
+
+        static TokenData NewLineIndentation(int column) {
+            return new TokenData {
+                Type = Token.NewLineIndentation,
+                Range = new TextFileRange {
+                    End = new TextPosition {Column = column}
+                }
+            };
+        }
+        static TokenData BlockStart(int column, ITokenBlock block = null) {
+            return new TokenData {
+                Type = Token.BlockStartIndentation,
+                Range = new TextFileRange {
+                    End = new TextPosition { Column = column }
+                },
+                Data = block
+            };
+        }
+        static TokenData BlockEnd(int column) {
+            return new TokenData {
+                Type = Token.BlockEndIndentation,
+                Range = new TextFileRange {
+                    End = new TextPosition { Column = column }
+                }
+            };
+        }
+
+        class Line : ITokenLine
+        {
+            public IList<TokenData> Tokens { get; } = new List<TokenData>();
+        }
+
+        class Block : ITokenBlock
+        {
+            public IList<ITokenLine> Lines { get; } = new List<ITokenLine>();
+        }
+
+        static readonly TestData[] GroupTests = new[] {
+            new TestData {
+                Input = new [] {
+                    Id(text: "if"), WS(), Id(text: "a"), NewLineIndentation(column: 4),
+                        Id(text: "&&"), WS(), Id(text: "b"), BlockStart(column: 4),
+                        Id(text: "print"), WS(), StringLiteral(text: "a"), NewLineIndentation(column: 1),
+                    Id(text: "else"), BlockStart(column: 4),
+                        Id(text: "print"), WS(), StringLiteral(text: "b"), BlockEnd(column: 1),
+                },
+                Output = new Block {
+                    Lines = {
+                        new Line {
+                            Tokens = {
+                                Id(text: "if"), WS(), Id(text: "a"), WS(), Id(text: "&&"), WS(), Id(text: "b"),
+                                BlockStart(column: 4, block: new Block {
+                                    Lines = {
+                                        new Line {
+                                            Tokens = { Id(text: "print"), WS(), StringLiteral(text: "a") }
+                                        }
+                                    }
+                                }), Id(text: "else"),
+                                BlockStart(column: 4, block: new Block {
+                                    Lines = {
+                                        new Line {
+                                            Tokens = { Id(text: "print"), WS(), StringLiteral(text: "b") }
+                                        }
+                                    }
+                                })
+                            }
+                        }
+                    }
+                }
+            }, 
+        };
+
+
+        [TestCaseSource(nameof(GroupTests))]
+        public void Group(TestData data) {
+            var grouping = new BlockLineGrouping();
+            var result = grouping.Group(data.Input);
+            AssertBlock(data.Output, result, label: "result");
+        }
+
+        void AssertBlock(ITokenBlock expected, ITokenBlock actual, string label) {
+            Assert.That(actual.Lines.Count, Is.EqualTo(expected.Lines.Count), $"{label}.Lines.Count");
+            for (var i = 0; i < actual.Lines.Count; i++) {
+                AssertLine(expected.Lines[i], actual.Lines[i], $"{label}.Lines[{i}]");
+            }
+        }
+
+        void AssertLine(ITokenLine expected, ITokenLine actual, string label) {
+            Assert.That(actual.Tokens.Count, Is.EqualTo(expected.Tokens.Count), $"{label}.Tokens.Count");
+            for (var i = 0; i < actual.Tokens.Count; i++) {
+                AssertToken(expected.Tokens[i], actual.Tokens[i], $"{label}.Tokens[{i}]");
+            }
+        }
+
+        void AssertToken(TokenData expected, TokenData actual, string label) {
+            Assert.That(actual.Type, Is.EqualTo(expected.Type), $"{label}.Type");
+            switch (actual.Type) {
+                case Token.BlockStartIndentation:
+                    Assert.That(actual.Range.End.Column, Is.EqualTo(expected.Range.End.Column));
+                    var actualBlock = actual.Data as ITokenBlock;
+                    var expectedBlock = expected.Data as ITokenBlock;
+                    AssertBlock(expectedBlock, actualBlock, $"{label}.Data");
+                    break;
+
+                case Token.StringLiteral:
+                case Token.NumberLiteral:
+                case Token.IdentifierLiteral:
+                case Token.OperatorLiteral:
+                    Assert.That(actual.Range.Text, Is.EqualTo(expected.Range.Text));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
