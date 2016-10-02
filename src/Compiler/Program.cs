08d72b67fd3c443f4c874bc93c52967f34a61612
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2016-10-02 23:28:51 +0200

    added build in msvc compiler invocation

--- src/Compiler/Program.cs
+++ src/Compiler/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using REC.Cpp;
 using REC.Intrinsic;
 using REC.Intrinsic.IO;
@@ -10,26 +12,85 @@ namespace REC
     {
         public Compiler() {
             InjectedScope = new Parser.Scope();
-            DeclarationConverter.BuildScope(InjectedScope,
+            DeclarationConverter.BuildScope(
+                InjectedScope,
                 new IntrinsicDict {
-                        Intrinsic.Types.U64Type.Get(),
-                        Intrinsic.Types.API.NumberLiteralType.Get(),
-                        PrintIntrinsic.Get(),
-                        SimpleMathIntrinsic<ulong, UlongMath>.Get()
+                    Intrinsic.Types.U64Type.Get(),
+                    Intrinsic.Types.API.NumberLiteralType.Get(),
+                    PrintIntrinsic.Get(),
+                    SimpleMathIntrinsic<ulong, UlongMath>.Get()
                 });
         }
 
         IScope InjectedScope { get; }
+
+        static string GetTempFileName(string basename = "", string extension = "tmp") {
+            return Path.GetTempPath() + basename + Guid.NewGuid().ToString() + '.' + extension;
+        }
 
         public void CompileFile(TextFile file) {
             var raw = Scanner.Scanner.ScanFile(file);
             var prepared = TokenPreparation.Apply(raw);
             var block = new BlockLineGrouping().Group(prepared);
             var ast = Parser.Parser.ParseBlock(block, InjectedScope);
-            using (var writer = File.CreateText(path: "compiled.cpp")) {
+            var cppFileName = GetTempFileName(Path.GetFileNameWithoutExtension(file.Filename), extension: "cpp"); 
+            //var cppFileName = Path.ChangeExtension(file.Filename, extension: "cpp"); // use this for debugging cpp output
+            using (var writer = File.CreateText(cppFileName)) {
                 var generator = new CppGenerator();
                 generator.Generate(writer, ast);
             }
+            RunCppCompiler(cppFileName, Path.ChangeExtension(file.Filename, extension: "exe"));
+        }
+
+        static string Escape(string path) {
+            return $"\"{path}\"";
+        }
+
+        void RunCppCompiler(string sourcePath, string finalPath) {
+            var shellEnv = @"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat";
+            var objPath = Path.ChangeExtension(sourcePath, extension: "obj");
+
+            var info = new ProcessStartInfo {
+                FileName = "cmd.exe",
+                Arguments = string.Join(
+                    separator: " ",
+                    value: new[] {
+                        "/c",
+                        "\"",
+                        Escape(shellEnv), "&&",
+                        "cl.exe",
+                        "/nologo", // clean output
+                        "/EHsc", // enable exceptions for STL
+                        #region optimized
+                        "/O2", // optimize
+                        "/MD", // multi threaded DLL
+                        "/DNDEBUG", // omit debug code
+                        #endregion
+                        #region debug
+                        //"/MDd", // multi threaded DLL debug
+                        //"/Zi", // complete debug infos  
+                        #endregion
+                        "/TP", // force to C++
+                        Escape($"/Fo{objPath}"),
+                        Escape(sourcePath),
+                        "/link", // here start the linker options
+                        Escape($"/PDB:{Path.ChangeExtension(finalPath, extension: "pdb")}"), // separate debug symbols
+                        Escape($"/out:{finalPath}"),
+                        "\""
+                    }),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                WorkingDirectory = Directory.GetCurrentDirectory()
+            };
+
+            var process = new Process {StartInfo = info};
+            process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+            process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
         }
     }
 
