7c315fd2c22eed535b0b84ccf5407f6c3aff8d0c
Richard Burkhardt <burkhardt@konsultaner.de>
2016-12-07 21:13:08 +0100

    added block comment scanner

--- src/Compiler/Scanner/CommentScanner.cs
+++ src/Compiler/Scanner/CommentScanner.cs
@@ -1,13 +1,60 @@
-﻿namespace REC.Scanner
+﻿using System;
+
+namespace REC.Scanner
 {
-    static class CommentScanner
+    public static class CommentScanner
     {
+        /**
+         * # single line
+         * 
+         * #<optional>#
+         * block comments
+         * #<optional># 
+         */
         public static bool Scan(TextInputRange range) {
             if (range.EndChar != '#') return false;
-            // TODO: allow for #(...) block comments
-            do {
+            do
+            {
                 range.Extend();
-            } while (range.IsEndValid && !range.IsEndNewline);
+            } while (range.IsEndValid && !range.IsEndNewline && !range.IsEndWhitespace && range.EndChar != '#');
+
+            // single line comment
+            if (range.IsEndNewline) return true;
+
+            // is block comment
+            if (range.EndChar == '#')
+            {
+                range.Extend();
+                string commentMarker = range.Text;
+                
+                while (range.IsEndValid && range.EndString(commentMarker.Length) != commentMarker)
+                {
+                    if (range.IsEndNewline)
+                    {
+                        range.NewLine();
+                    }
+                    else
+                    {
+                        range.Extend();
+                    }
+                }
+
+                if(range.EndString(commentMarker.Length) != commentMarker)
+                {
+                    throw new Exception("Line Comment not Escaped.");
+                }
+                else
+                {
+                    range.Extend(commentMarker.Length);
+                }
+            }
+            else
+            {
+                do
+                {
+                    range.Extend();
+                } while (range.IsEndValid && !range.IsEndNewline);
+            }
             return true;
         }
     }
