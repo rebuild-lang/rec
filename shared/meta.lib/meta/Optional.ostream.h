05c8e8970dcd7f74cd9d2ad658d4f3ddbeb23903
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2019-06-23 21:31:48 +0200

    fixed ostream for packed optionals

--- shared/meta.lib/meta/Optional.ostream.h
+++ shared/meta.lib/meta/Optional.ostream.h
@@ -5,7 +5,7 @@
 
 template<typename Char, typename CharTraits, class T>
 auto operator<<(::std::basic_ostream<Char, CharTraits>& out, const meta::Optional<T>& opt)
-    -> decltype(out << std::declval<T>()) {
+    -> decltype(out << std::declval<meta::Optional<T>>().value()) {
 
     if (!opt) return out << "<Empty>";
     return out << opt.value();
